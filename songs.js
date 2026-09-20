4a93dab0a9d6500922b5efd4db56dfb8866135d0
tuanhung2309077-gif <tuanhung2309077@gmail.com>
2026-09-20 18:10:37 +0700

    Update comment format for song entries

--- songs.js
+++ songs.js
@@ -1,7 +1,7 @@
 // Thêm bài hát của bạn vào giữa dấu [ và ] bên dưới, mỗi bài một dòng, nhớ có dấu phẩy ở cuối.
 //
 // Bài từ YouTube: dán nguyên link video vào y:
-//   {n:'🎶 Tên bài', y:'https://www.youtube.com/watch?v=XXXXXXXXXXX'},
+//   {n:'🎶 nếu như ta chẳng còn', https://youtu.be/rYWLIJB214Q?si=OGMg8GyRfgko-jOu},
 //
 // Bài từ file mp3 (để trong thư mục "music" của kho GitHub):
 //   {n:'🎶 Tên bài', f:'music/ten-file.mp3'},
